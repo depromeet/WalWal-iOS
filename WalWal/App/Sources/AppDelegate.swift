2f29c55ab4d7341dbe34590ec74654d66209bf37
ir.__.si <cocoa9805@naver.com>
2024-06-22 16:25:30 +0900

    infra: initial build

---
+++ WalWal/App/Sources/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  WalWal
+//
+//  Created by 조용인 on 6/22/24.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    
+    return true
+  }
+
+}
+
---
+++ WalWal/App/Sources/SceneDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  SceneDelegate.swift
+//  WalWal
+//
+//  Created by 조용인 on 6/22/24.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+  var window: UIWindow?
+
+
+  func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+    
+    guard let _ = (scene as? UIWindowScene) else { return }
+  }
+
+}
+
