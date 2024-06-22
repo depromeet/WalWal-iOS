839995135d7d31649646a32940a8e4f2988987a8
ir.__.si <cocoa9805@naver.com>
2024-06-22 15:03:52 +0900

    infra: dummy app 추가

---
+++ WalWal/App/Project.swift
@@ -0,0 +1,36 @@
+//
+//  Project.swift
+//  ProjectDescriptionHelpers
+//
+//  Created by 조용인 on 6/22/24.
+//
+
+import ProjectDescription
+import ProjectDescriptionHelpers
+import DependencyPlugin
+
+let project = Project(
+    name: "App",
+    targets: [
+        Target(
+            name: "App",
+            platform: .iOS,
+            product: .app,
+            bundleId: "olderStoneBed.io.walwal",
+            infoPlist: InfoPlist.extendingDefault(
+                with:
+                    [
+                        "CFBundleDevelopmentRegion": "ko_KR",
+                        "CFBundleShortVersionString": "1.0",
+                        "CFBundleVersion": "1",
+                        "UILaunchStoryboardName": "LaunchScreen"
+                    ]
+            ),
+            sources: ["Sources/**"],
+            resources: ["Resources/**"],
+            dependencies: [
+
+            ]
+        )
+    ]
+)
