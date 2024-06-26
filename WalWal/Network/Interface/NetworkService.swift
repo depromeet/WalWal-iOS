dd29c3d83eb4150f47c09581d6c273999ad95963
Zoe <mam07065@naver.com>
2024-06-27 02:15:39 +0900

    feat: NetworkService Interface

--- WalWal/Network/Interface/NetworkService.swift
+++ WalWal/Network/Interface/NetworkService.swift
@@ -7,3 +7,20 @@
 //
 
 import Foundation
+
+// MARK: - NetworkError
+/// NetworkError: 네트워크 요청 중 발생할 수 있는 에러 타입을 정의
+enum NetworkError: Error {
+    case serverError(statusCode: Int)
+    case decodingError(Error)
+    case unknown(Error)
+}
+
+// MARK: - NetworkService
+/// NetworkService: 네트워크 서비스 프로토콜 정의
+/// 네트워크 요청을 수행하는 메서드를 정의합니다.
+protocol NetworkServiceProtocol {
+    /// request(:) 메서드는 APIEndpoint 프로토콜을 준수하는 엔드포인트를 받아 Single<T> 타입의 Observable을 반환합니다.
+    /// 공통되는 엔티티를 사용하기 위해 BaseResponse를 사용합니다.
+    func request<T: Decodable>(endpoint: APIEndpoint) -> Single<T>
+}
