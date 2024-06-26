82df5850676e705c695df9b0293b933feb72f6e7
Zoe <mam07065@naver.com>
2024-06-27 02:16:18 +0900

    feat: APIEndPoint 정의

--- WalWal/Network/Interface/APIEndpoint.swift
+++ WalWal/Network/Interface/APIEndpoint.swift
@@ -1,1 +1,13 @@
+import Foundation
 
+import RxAlamofire
+
+/// APIEndpoint 프로토콜
+/// API 엔드포인트를 정의하는 용도로 사용됩니다.
+protocol APIEndpoint {
+    var baseURL: URL { get }
+    var path: String { get }
+    var method: HTTPMethod { get }
+    var parameters: [String: Any]? { get }
+    var headers: [String: String]? { get }
+}
