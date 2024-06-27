13daa3c1e3eb1d7730c9baf9bd70ce3452200908
Zoe <mam07065@naver.com>
2024-06-27 15:19:19 +0900

    feat: EndPoint 구현부

---
+++ WalWal/Network/Implement/APIEndpointImpl.swift
@@ -0,0 +1,50 @@
+//
+//  APIEndpointImpl.swift
+//  NetworkImp
+//
+//  Created by 이지희 on 6/27/24.
+//  Copyright © 2024 olderStoneBed.io. All rights reserved.
+//
+
+import Foundation
+
+import RxAlamofire
+import Alamofire
+
+///URLRequestConvertible 프로토콜 구현부 :
+/// URLRequest 객체를 생성하는 방법을 정의.
+extension APIEndpoint: URLRequestConvertible {
+    func asURLRequest() throws -> URLRequest {
+        /// url  생성 후, path, method를 사용하여 urlRequest 생성
+        let url = try baseURL.asURL()
+        var urlRequest = try URLRequest(url: url.appendingPathComponent(path), method: method)
+        
+        /// HTTPHeader
+        urlRequest.setValue(ContentType.json.rawValue, forHTTPHeaderField: HTTPHeaderField.contentType.rawValue)
+        
+        /// parameters 프로퍼티에 따라 query parameter 또는 request body 생성
+        switch parameters {
+        case .query(let request):
+            let params = request?.toDictionary() ?? [:]
+            let queryParams = params.map { URLQueryItem(name: $0.key, value: "\($0.value)") }
+            var components = URLComponents(string: url.appendingPathComponent(path).absoluteString)
+            components?.queryItems = queryParams
+            urlRequest.url = components?.url
+        case .body(let request):
+            let params = request?.toDictionary() ?? [:]
+            urlRequest.httpBody = try JSONSerialization.data(withJSONObject: params, options: [])
+        }
+        
+        return urlRequest
+    }
+}
+
+
+extension Encodable {
+    func toDictionary() -> [String: Any] {
+        guard let data = try? JSONEncoder().encode(self),
+              let jsonData = try? JSONSerialization.jsonObject(with: data),
+              let dictionaryData = jsonData as? [String: Any] else { return [:] }
+        return dictionaryData
+    }
+}
