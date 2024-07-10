57cd1e1545a8b07785c2c83fa372e1a8ff204a3c
ji-yeon224 <69784492+ji-yeon224@users.noreply.github.com>
2024-07-10 23:42:59 +0900

    feat: 로그인 UI 레이아웃 구현

--- WalWal/Features/Auth/AuthPresenter/View/AuthView.swift
+++ WalWal/Features/Auth/AuthPresenter/View/AuthView.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AuthenticationServices
 
 import ReactorKit
 import Then
@@ -20,23 +21,54 @@ final public class AuthViewController: UIViewController {
   
   // MARK: UI
   
+  private let rootContainer = UIView()
+  private let imageView = UIImageView().then {
+    $0.backgroundColor = .lightGray
+  }
+  private let titleLabel = UILabel().then {
+    $0.text = "왈왈에서 매일 만나요"
+    $0.textColor = .black
+    $0.font = .boldSystemFont(ofSize: 24)
+  }
+  private let subTitleLabel = UILabel().then {
+    $0.text = "세상 모든 반려동물을 한자리에서!"
+    $0.textColor = .black
+    $0.font = .systemFont(ofSize: 14)
+  }
+  private let appleLoginButton = ASAuthorizationAppleIDButton(authorizationButtonType: .continue, authorizationButtonStyle: .black)
+  
   // MARK: - View LifeCycle
   
   override public func viewDidLoad() {
     super.viewDidLoad()
-    view.backgroundColor = .white
     setAttribute()
     setLayout()
   }
   
   // MARK: - Layout
   
+  override public func viewDidLayoutSubviews() {
+    super.viewDidLayoutSubviews()
+    rootContainer.pin.all(view.pin.safeArea)
+    rootContainer.flex.layout()
+  }
+  
   private func setAttribute() {
-    
+    view.backgroundColor = .white
+    view.addSubview(rootContainer)
   }
   
   private func setLayout() {
-    
+    rootContainer.flex.justifyContent(.spaceBetween).define { flex in
+      flex.addItem().alignItems(.center).paddingTop(35%).define { flex in
+        flex.addItem(imageView).width(55%).aspectRatio(1.0)
+        flex.addItem(titleLabel).marginTop(10)
+        flex.addItem(subTitleLabel).marginTop(10)
+      }
+      flex.addItem().marginBottom(40).define { flex in
+        flex.addItem(appleLoginButton).marginHorizontal(20).height(56)
+      }
+    }
   }
   
 }
