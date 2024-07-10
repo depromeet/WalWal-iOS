38c1873b3a59a0843a61e3bb44b064ca8b4db529
ir.__.si <cocoa9805@naver.com>
2024-07-10 11:26:40 +0900

    feat: DependencyFactory Implement 초안 작성 및 사용가이드

---
+++ WalWal/DependencyFactory/Implement/DependencyFactoryImp.swift
@@ -0,0 +1,29 @@
+//
+//  DependencyFactoryImp.swift
+//  DependencyFactory
+//
+//  Created by 조용인 on 7/10/24.
+//  Copyright © 2024 olderStoneBed.io. All rights reserved.
+//
+import Foundation
+import UIKit
+
+import DependencyFactory
+// MARK: - 추가되는 Feature에 따라 import되는 Interface와 Implement를 작성해주세요.
+// (이곳은 Interface와 Implement 동시에 의존성을 갖습니다.)
+// Ex.
+// import AuthDomain
+// import AuthDomainImp
+
+// import AuthData
+// import AuthDataImp
+// 등등...
+
+
+public class DependencyFactoryImp: DependencyFactory {
+  // MARK: - 추가되는 Feature에 따라 Dependency를 생성 및 주입하는 함수의 구현부를 작성해주세요.
+  /*
+  public func injectAuthData() -> AuthData { ... }
+  public func injectAuthDomain() -> AuthDomain { ... }
+   */
+}
