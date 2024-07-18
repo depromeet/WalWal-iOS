5629be8eb71e48137750e9ce480d87c21ce506a3
ir.__.si <cocoa9805@naver.com>
2024-07-18 14:22:48 +0900

    feat: #24 Splash에 대한 DependencyFactorey Protocol 추가 및 구현

--- WalWal/DependencyFactory/Interface/DependencyFactory.swift
+++ WalWal/DependencyFactory/Interface/DependencyFactory.swift
@@ -8,9 +8,14 @@
 import UIKit
 
 import BaseCoordinator
+import AppCoordinator
 import SampleAppCoordinator
 import SampleAuthCoordinator
 import SampleHomeCoordinator
+
+import SplashData
+import SplashDomain
+import SplashPresenter
 
 import SampleData
 import SampleDomain
@@ -20,16 +25,22 @@ public protocol DependencyFactory {
   
   // MARK: - 추가되는 Feature에 따라 Dependency를 생성 및 주입하는 함수를 추가해주새요
   
+  /// Sample Features
   func makeSampleAuthData() -> SampleAuthRepository
   
   func makeSampleSignInUsecase() -> SampleSignInUseCase
   func makeSampleSignUpUsecase() -> SampleSignUpUseCase
   
-  func makeSampleAppCoordinator(navigationController: UINavigationController) -> any SampleAppCoordinator
-  
   func makeSampleReactor(coordinator: any SampleAppCoordinator) -> any SampleReactor
   func makeSampleViewController<T: SampleReactor>(reactor: T) -> any SampleViewController
   
+  func makeSplashReactor(coordinator: any AppCoordinator) -> any SplashReactor
+  func makeSplashViewController<T: SplashReactor>(reactor: T) -> any SplashViewController
+  
+  func makeAppCoordinator(navigationController: UINavigationController) -> any AppCoordinator
+
+  /// Sample Coordinaotrs
+  func makeSampleAppCoordinator(navigationController: UINavigationController) -> any SampleAppCoordinator
   
   func makeSampleAuthCoordinator(navigationController: UINavigationController, parentCoordinator: any BaseCoordinator) -> any SampleAuthCoordinator
   
