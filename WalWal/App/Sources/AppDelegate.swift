2dc2f6af1713190a29fe042722ef62a741c0bb0f
ir.__.si <cocoa9805@naver.com>
2024-07-18 20:53:26 +0900

    fix: #24 AppDelegate 접근제한자 수정

--- WalWal/App/Sources/AppDelegate.swift
+++ WalWal/App/Sources/AppDelegate.swift
@@ -10,7 +10,7 @@ import DependencyFactory
 import DependencyFactoryImp
 
 @main
-class AppDelegate: UIResponder, UIApplicationDelegate {
+final class AppDelegate: UIResponder, UIApplicationDelegate {
   var window: UIWindow?
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
     let window = UIWindow(frame: UIScreen.main.bounds)
