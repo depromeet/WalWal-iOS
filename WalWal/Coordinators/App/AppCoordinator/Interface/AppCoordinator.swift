a377174e3f96ff15eebe4b0d0ebc38a1bc51e584
ir.__.si <cocoa9805@naver.com>
2024-07-18 10:47:25 +0900

    feat: #24 AppCoordinator scaffold 생성

---
+++ WalWal/Coordinators/App/AppCoordinator/Interface/AppCoordinator.swift
@@ -0,0 +1,20 @@
+//
+//  AppCoordinatorInterface.swift
+//
+//  App
+//
+//  Created by 조용인
+//
+
+import UIKit
+import BaseCoordinator
+
+public enum AppCoordinatorAction: ParentAction {
+  
+}
+
+public enum AppCoordinatorFlow: CoordinatorFlow {
+  
+}
+
+public protocol AppCoordinator: BaseCoordinator { }
---
+++ WalWal/Coordinators/App/AppCoordinator/Project.swift
@@ -0,0 +1,30 @@
+//
+//  AppCoordinatorProject.swift
+//
+//  App
+//
+//  Created by 조용인
+//
+
+import ProjectDescription
+import ProjectDescriptionHelpers
+import DependencyPlugin
+
+let project = Project.invertedDualTargetProject(
+  name: "AppCoordinator",
+  platform: .iOS,
+  iOSTargetVersion: "15.0.0",
+  interfaceDependencies: [
+    .Coordinator.Base.Interface,
+    
+    .ThirdParty.RxSwift,
+    .ThirdParty.RxCocoa
+  ],
+  implementDependencies: [
+    .Coordinator.Base.Interface,
+    .DependencyFactory.Interface,
+    
+    .ThirdParty.RxSwift,
+    .ThirdParty.RxCocoa
+  ]
+)
