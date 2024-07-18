b9b30b782335b5f3e8a66d57c43621371add01d5
ir.__.si <cocoa9805@naver.com>
2024-07-18 15:50:40 +0900

    chore: indent 수정

--- WalWal/Features/Splash/SplashPresenter/Implement/Reactors/SplashReactorImp.swift
+++ WalWal/Features/Splash/SplashPresenter/Implement/Reactors/SplashReactorImp.swift
@@ -14,25 +14,25 @@ import ReactorKit
 import RxSwift
 
 public final class SplashReactorImp: SplashReactor {
-    public typealias Action = SplashReactorAction
-    public typealias Mutation = SplashReactorMutation
-    public typealias State = SplashReactorState
-
-    public let initialState: State
-    public let coordinator: any AppCoordinator
+  public typealias Action = SplashReactorAction
+  public typealias Mutation = SplashReactorMutation
+  public typealias State = SplashReactorState
+  
+  public let initialState: State
+  public let coordinator: any AppCoordinator
+  
+  public init(
+    coordinator: any AppCoordinator
+  ) {
+    self.coordinator = coordinator
+    self.initialState = State()
+  }
+  
+  public func mutate(action: Action) -> Observable<Mutation> {
     
-    public init(
-        coordinator: any AppCoordinator
-    ) {
-        self.coordinator = coordinator
-        self.initialState = State()
-    }
+  }
+  
+  public func reduce(state: State, mutation: Mutation) -> State {
     
-    public func mutate(action: Action) -> Observable<Mutation> {
-      
-    }
-    
-    public func reduce(state: State, mutation: Mutation) -> State {
-        
-    }
+  }
 }
