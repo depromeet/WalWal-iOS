54686e998937e4afc7cd8ce63f36b82a7bb66ff8
ir.__.si <cocoa9805@naver.com>
2024-07-23 19:03:23 +0900

    feat: #32 DesignSystemDemoApp 세팅

---
+++ WalWal/DesignSystem/DemoApp/Sources/DesignSystemViewController.swift
@@ -0,0 +1,48 @@
+//
+//  DesignSystemViewController.swift
+//  DesignSystemDemoApp
+//
+//  Created by 조용인 on 7/23/24.
+//  Copyright © 2024 olderStoneBed.io. All rights reserved.
+//
+
+import UIKit
+
+fileprivate enum DesignSystemType: String, CaseIterable {
+  case calender
+}
+
+final class DesignSystemViewController: UITableViewController {
+  
+  private var designSystems: [DesignSystemType] = DesignSystemType.allCases
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+  }
+  
+  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return designSystems.count
+  }
+  
+  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+    cell.textLabel?.text = self.designSystems[indexPath.item].rawValue
+    return cell
+  }
+  
+  override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+    return 50
+  }
+  
+  override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    let designSystem = self.designSystems[indexPath.item]
+    switch designSystem {
+    case .calender:
+      self.navigationController?.pushViewController(WalWalCalenderDemoViewController(), animated: true)
+    default: return
+    }
+  }
+}
+
+
---
+++ WalWal/DesignSystem/DemoApp/Sources/WalWalCalendar/WalWalCalenderDemoViewController.swift
@@ -0,0 +1,9 @@
+//
+//  WalWalCalenderDemoViewController.swift
+//  DesignSystemDemoApp
+//
+//  Created by 조용인 on 7/23/24.
+//  Copyright © 2024 olderStoneBed.io. All rights reserved.
+//
+
+import Foundation
