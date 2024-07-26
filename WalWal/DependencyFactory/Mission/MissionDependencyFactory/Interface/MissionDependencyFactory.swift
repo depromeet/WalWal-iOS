e74f29de52df1dd6e7e8de63f33ffe70bebc61f3
Zoe <mam07065@naver.com>
2024-07-27 05:17:49 +0900

    feat: #23 구조 적용

---
+++ WalWal/DependencyFactory/Mission/MissionDependencyFactory/Interface/MissionDependencyFactory.swift
@@ -0,0 +1,21 @@
+//
+//  MissionDependencyFactoryInterface.swift
+//
+//  Mission
+//
+//  Created by 이지희
+//
+
+import UIKit
+import BaseCoordinator
+import MissionCoordinator
+
+import MissionData
+import MissionDomain
+import MissionPresenter
+
+public protocol MissionDependencyFactory {
+  func makeMissionReactor(coordinator: any MissionCoordinator) -> any MissionReactor
+  func makeMissionViewController<T: MissionReactor>(reactor: T) -> any MissionViewController
+  func makeMissionCoordinator(navigationController: UINavigationController) -> any MissionCoordinator
+}
