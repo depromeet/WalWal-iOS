8695a25a0c7fba12dc4915ea2e1d1e1faf762f16
Zoe <mam07065@naver.com>
2024-07-27 19:40:31 +0900

    feat:#23 데이터 계층

---
+++ WalWal/Features/Mission/MissionData/Implement/Repository/MissionRepositoryImp.swift
@@ -0,0 +1,29 @@
+//
+//  MissionRepositoryImp.swift
+//  MissionData
+//
+//  Created by 이지희 on 7/27/24.
+//  Copyright © 2024 olderStoneBed.io. All rights reserved.
+//
+
+import Foundation
+import WalWalNetwork
+
+import RxSwift
+
+public final class MissionRepositoryImp: MissionRepository {
+  
+  private let networkService: NetworkServiceProtocol
+  
+  public init(networkService: NetworkServiceProtocol) {
+    self.networkService = networkService
+  }
+  
+  public func getMissionInfo() -> RxSwift.Single<MissionInfoDTO> {
+    let endpoint = MissionEndpoint<MissionInfoDTO>.getMissionInfo
+    return networkService.request(endpoint: endpoint)
+      .compactMap { $0 }
+      .asObservable()
+      .asSingle()
+  }
+}
