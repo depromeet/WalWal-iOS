10dccf2894b30ff58753990855822bdcb8ab80d5
ir.__.si <cocoa9805@naver.com>
2024-07-29 10:41:55 +0900

    feat: #39 네비게이션바 아이템 설정 구현

---
+++ WalWal/DesignSystem/Sources/WalWalNavigationBar/Components/NavigationBarItem.swift
@@ -0,0 +1,38 @@
+//
+//  NavigationBarItem.swift
+//  DesignSystem
+//
+//  Created by 조용인 on 7/29/24.
+//  Copyright © 2024 olderStoneBed.io. All rights reserved.
+//
+
+import UIKit
+import ResourceKit
+
+import RxSwift
+
+public final class NavigationBarItem: UIButton {
+  
+  private let type: NavigationBarItemType
+  
+  public init(_ type: NavigationBarItemType) {
+    self.type = type
+    super.init(frame: .zero)
+    configureIcon(icon: type.icon)
+  }
+  
+  @available (*, unavailable)
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  private func configureIcon(icon: UIImage?) {
+    guard let tintedImage = type.icon?.withRenderingMode(.alwaysTemplate) else {
+      self.setImage(nil, for: .normal)
+      return
+    }
+    
+    self.setImage(tintedImage, for: .normal)
+  }
+}
+
