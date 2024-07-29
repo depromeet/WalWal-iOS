7159374494136c8a89971c385d94149388e6314e
ir.__.si <cocoa9805@naver.com>
2024-07-29 14:19:06 +0900

    feat: #39 샘플 코드 수정 및 bind 추가

--- WalWal/DesignSystem/DemoApp/Sources/WalWalNavigationBar/WalWalNavigationBarDemoViewController.swift
+++ WalWal/DesignSystem/DemoApp/Sources/WalWalNavigationBar/WalWalNavigationBarDemoViewController.swift
@@ -31,35 +31,41 @@ final class WalWalNavigationBarDemoViewController: UIViewController {
     colorType: .normal
   )
   
-  private let containerView = UIView()
+  private let containerView = UIView().then {
+    $0.backgroundColor = ResourceKitAsset.Colors.white.color
+  }
   
   // MARK: - Properties
   
   private let disposeBag = DisposeBag()
   
-  // MARK: - Lifecycle
+  // MARK: - Initializers
   
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    self.navigationController?.isNavigationBarHidden = true
-    self.navigationController?.navigationItem.title = "WalWal"
-    self.view.addSubview(self.containerView)
-    self.configureLayouts()
-    self.view.backgroundColor = .white
-    
+  public init() {
+    super.init(nibName: nil, bundle: nil)
+    configureAttributes()
+    configureLayouts()
+    bind()
   }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  // MARK: - Lifecycle
   
   override func viewDidLayoutSubviews() {
     super.viewDidLayoutSubviews()
-    self.containerView.pin.all(self.view.pin.safeArea)
-    self.containerView.flex.layout()
+    self.containerView.pin
+      .all(self.view.pin.safeArea)
+    self.containerView.flex
+      .layout()
   }
   
   // MARK: - Methods
   
   private func bind() {
     navigaionBar.leftItems?[0].rx.tapped
-      .debug("WalWalTouchArea tapped With navigation")
       .subscribe(with: self, onNext: { owner, _ in
         owner.navigationController?.popViewController(animated: true)
       })
@@ -70,9 +76,14 @@ final class WalWalNavigationBarDemoViewController: UIViewController {
 
 // MARK: - Private Methods
 
-extension WalWalNavigationBarDemoViewController {
-  private func configureLayouts() {
-    self.containerView.backgroundColor = ResourceKitAsset.Colors.walwalOrange.color
+private extension WalWalNavigationBarDemoViewController {
+  func configureAttributes() {
+    self.navigationController?.isNavigationBarHidden = true
+    self.navigationController?.navigationItem.title = "WalWal"
+  }
+  
+  func configureLayouts() {
+    self.view.addSubview(self.containerView)
     self.containerView.flex
       .direction(.column)
       .define { flex in
