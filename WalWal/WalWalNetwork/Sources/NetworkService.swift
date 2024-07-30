abf4beb810446c35cb41e29affb5a23182666285
Zoe <mam07065@naver.com>
2024-07-30 17:23:38 +0900

    feat: #59 인터페이스에 업로드 함수 추가

--- WalWal/WalWalNetwork/Sources/NetworkService.swift
+++ WalWal/WalWalNetwork/Sources/NetworkService.swift
@@ -18,4 +18,5 @@ public protocol NetworkServiceProtocol {
   /// request(:) 메서드는 APIEndpoint 프로토콜을 준수하는 엔드포인트를 받아 Single<T> 타입의 Observable을 반환합니다.
   /// 공통되는 엔티티를 사용하기 위해 BaseResponse를 사용합니다.
   func request<E: APIEndpoint>(endpoint: E) -> Single<E.ResponseType?> where E: APIEndpoint
+  func upload<E: APIEndpoint> (endpoint: E, image: UIImage) -> Single<Bool> where E: APIEndpoint
 }
