132c38e7a3c4d26dc561df3ede393df95ea0ad2e
ir.__.si <cocoa9805@naver.com>
2024-07-31 23:25:58 +0900

    feat: 2024.07.31 리소스킷 업데이트

--- WalWal/ResourceKit/Sources/Font.swift
+++ WalWal/ResourceKit/Sources/Font.swift
@@ -16,12 +16,13 @@ extension ResourceKitFontFamily {
     public static let H1 = UIFont.systemFont(ofSize: 32, weight: .bold)
     public static let H2 = UIFont.systemFont(ofSize: 28, weight: .bold)
     public static let H3 = UIFont.systemFont(ofSize: 24, weight: .bold)
-    public static let H4 = UIFont.systemFont(ofSize: 18, weight: .bold)
-    public static let H5 = UIFont.systemFont(ofSize: 16, weight: .bold)
-    public static let B1 = UIFont.systemFont(ofSize: 15, weight: .bold)
-    public static let B2 = UIFont.systemFont(ofSize: 14, weight: .bold)
-    public static let B3 = UIFont.systemFont(ofSize: 11, weight: .bold)
-    public static let Caption = UIFont.systemFont(ofSize: 10, weight: .bold)
+    public static let H4 = UIFont.systemFont(ofSize: 20, weight: .bold)
+    public static let H5 = UIFont.systemFont(ofSize: 18, weight: .bold)
+    public static let H6 = UIFont.systemFont(ofSize: 16, weight: .bold)
+    public static let H7 = UIFont.systemFont(ofSize: 14, weight: .bold)
+    public static let B1 = UIFont.systemFont(ofSize: 14, weight: .regular)
+    public static let B2 = UIFont.systemFont(ofSize: 12, weight: .regular)
+    public static let Caption = UIFont.systemFont(ofSize: 10, weight: .regular)
   }
   
   /// 사용 방법
@@ -30,17 +31,20 @@ extension ResourceKitFontFamily {
     public static let H1 = UIFont.systemFont(ofSize: 32, weight: .bold)
     public static let H2 = UIFont.systemFont(ofSize: 28, weight: .bold)
     public static let H3 = UIFont.systemFont(ofSize: 24, weight: .bold)
-    public static let H4 = UIFont.systemFont(ofSize: 18, weight: .bold)
-    public static let H5 = UIFont.systemFont(ofSize: 16, weight: .bold)
-    public static let B1 = UIFont.systemFont(ofSize: 15, weight: .bold)
-    public static let B2 = UIFont.systemFont(ofSize: 14, weight: .bold)
-    public static let B3 = UIFont.systemFont(ofSize: 11, weight: .bold)
-    public static let Caption = UIFont.systemFont(ofSize: 10, weight: .bold)
+    public static let H4 = UIFont.systemFont(ofSize: 20, weight: .bold)
+    public static let H5 = UIFont.systemFont(ofSize: 18, weight: .bold)
+    public static let H6 = UIFont.systemFont(ofSize: 16, weight: .bold)
+    public static let H7 = UIFont.systemFont(ofSize: 14, weight: .bold)
+    public static let B1 = UIFont.systemFont(ofSize: 14, weight: .regular)
+    public static let B2 = UIFont.systemFont(ofSize: 12, weight: .regular)
+    public static let Caption = UIFont.systemFont(ofSize: 10, weight: .regular)
   }
 }
 
 extension ResourceKitFontFamily.LotteriaChab {
   /// 사용 방법
   /// $0.font = ResourceKitFontFamily.LotteriaChab.H1
-  public static let H1 = Self.regular.font(size: 32)
+  public static let H1 = Self.regular.font(size: 48)
+  public static let H2 = Self.regular.font(size: 34)
+  public static let H3 = Self.regular.font(size: 24)
 }
