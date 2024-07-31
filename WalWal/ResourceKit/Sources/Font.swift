371c93f2d6e6decd40abaaa4180dd8d4a2049d73
ir.__.si <cocoa9805@naver.com>
2024-08-01 01:02:22 +0900

    fix: 2024.07.31 Font 수정

--- WalWal/ResourceKit/Sources/Font.swift
+++ WalWal/ResourceKit/Sources/Font.swift
@@ -17,9 +17,18 @@ extension ResourceKitFontFamily {
     public static let H2 = UIFont.systemFont(ofSize: 28, weight: .bold)
     public static let H3 = UIFont.systemFont(ofSize: 24, weight: .bold)
     public static let H4 = UIFont.systemFont(ofSize: 20, weight: .bold)
-    public static let H5 = UIFont.systemFont(ofSize: 18, weight: .bold)
-    public static let H6 = UIFont.systemFont(ofSize: 16, weight: .bold)
-    public static let H7 = UIFont.systemFont(ofSize: 14, weight: .bold)
+    public struct H5 {
+      public static let B = UIFont.systemFont(ofSize: 18, weight: .bold)
+      public static let M = UIFont.systemFont(ofSize: 18, weight: .medium)
+    }
+    public struct H6 {
+      public static let B = UIFont.systemFont(ofSize: 16, weight: .bold)
+      public static let M = UIFont.systemFont(ofSize: 16, weight: .medium)
+    }
+    public struct H7 {
+      public static let B = UIFont.systemFont(ofSize: 14, weight: .bold)
+      public static let M = UIFont.systemFont(ofSize: 14, weight: .medium)
+    }
     public static let B1 = UIFont.systemFont(ofSize: 14, weight: .regular)
     public static let B2 = UIFont.systemFont(ofSize: 12, weight: .regular)
     public static let Caption = UIFont.systemFont(ofSize: 10, weight: .regular)
@@ -28,16 +37,11 @@ extension ResourceKitFontFamily {
   /// 사용 방법
   /// $0.font = ResourceKitFontFamily.EN.H1
   public struct EN {
-    public static let H1 = UIFont.systemFont(ofSize: 32, weight: .bold)
-    public static let H2 = UIFont.systemFont(ofSize: 28, weight: .bold)
-    public static let H3 = UIFont.systemFont(ofSize: 24, weight: .bold)
-    public static let H4 = UIFont.systemFont(ofSize: 20, weight: .bold)
-    public static let H5 = UIFont.systemFont(ofSize: 18, weight: .bold)
-    public static let H6 = UIFont.systemFont(ofSize: 16, weight: .bold)
-    public static let H7 = UIFont.systemFont(ofSize: 14, weight: .bold)
+    public static let H1 = UIFont.systemFont(ofSize: 16, weight: .bold)
+    public static let H2 = UIFont.systemFont(ofSize: 14, weight: .semibold)
+    public static let H3 = UIFont.systemFont(ofSize: 14, weight: .medium)
     public static let B1 = UIFont.systemFont(ofSize: 14, weight: .regular)
-    public static let B2 = UIFont.systemFont(ofSize: 12, weight: .regular)
-    public static let Caption = UIFont.systemFont(ofSize: 10, weight: .regular)
+    public static let Caption = UIFont.systemFont(ofSize: 12, weight: .regular)
   }
 }
 
