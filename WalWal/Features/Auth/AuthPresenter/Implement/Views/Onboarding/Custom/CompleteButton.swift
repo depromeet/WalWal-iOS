9bb0e64460d9a2081b62ad5f93ae1a6d46acf09b
ji-yeon224 <69784492+ji-yeon224@users.noreply.github.com>
2024-07-31 17:46:02 +0900

    chore: #17 OnboardingView Auth로 이동

---
+++ WalWal/Features/Auth/AuthPresenter/Implement/Views/Onboarding/Custom/CompleteButton.swift
@@ -0,0 +1,48 @@
+//
+//  CompleteButton.swift
+//  OnboardingPresenterView
+//
+//  Created by Jiyeon on 7/15/24.
+//  Copyright © 2024 olderStoneBed.io. All rights reserved.
+//
+
+import UIKit
+
+/// Onboarding에서 재사용하기 위한 다음 버튼
+///
+/// - Parameters:
+///   - title: 버튼 타이틀 텍스트로 기본 값으로 "다음"을 가지고 있습니다.
+///   - isEnable: 버튼 활성화 여부로, 초기에 활성화 여부를 설정할 수 있습니다.
+final class CompleteButton: UIButton {
+  
+  /// 버튼 활성화 값에 따라 백그라운드 색 변경되도록 설정
+  ///
+  /// 사용예시
+  /// ```swift
+  /// nextButton.isEnable = true
+  /// ```
+  override var isEnabled: Bool {
+    didSet {
+      if self.isEnabled {
+        backgroundColor = .systemOrange
+      } else {
+        backgroundColor = .lightGray
+      }
+    }
+  }
+  
+  // MARK: - Initialize
+  
+  init(title: String = "다음", isEnable: Bool) {
+    super.init(frame: .zero)
+    isEnabled = isEnable
+    layer.cornerRadius = 10
+    setTitle(title, for: .normal)
+    titleLabel?.font = .systemFont(ofSize: 18, weight: .bold)
+  }
+  
+  @available(*, unavailable)
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+}
---
+++ WalWal/Features/Auth/AuthPresenter/Implement/Views/Onboarding/OnboardingViewImp.swift
@@ -0,0 +1,144 @@
+//
+//  OnboardingViewImp.swift
+//  AuthPresenter
+//
+//  Created by Jiyeon on 7/31/24.
+//  Copyright © 2024 olderStoneBed.io. All rights reserved.
+//
+
+import UIKit
+
+import Then
+import PinLayout
+import FlexLayout
+import ReactorKit
+import RxSwift
+import RxCocoa
+
+public final class OnboardingViewControllerImp<R: AuthReactor>: UIViewController, AuthViewController, UIScrollViewDelegate {
+  public var disposeBag = DisposeBag()
+  private var onboardingReactor: R
+  
+  // MARK: - Initialize
+  
+  private let rootContainer = UIView()
+  private let descriptionView1 = DescriptionView(mainTitle: "반려동물과 함께하는\n데일리 미션", subText: "반려동물과 함께 미션을 수행해요", image: nil)
+  private let descriptionView2 = DescriptionView(mainTitle: "귀여운 반려동물\n피드에서 모아보세요!", subText: "다양한 반려동물을 한 눈에 발견해요", image: nil)
+  private let descriptionView3 = DescriptionView(mainTitle: "매일 수행한 미션\n언제든지 꺼내봐요!", subText: "반려동물과의 함께한 추억을 기억해요", image: nil)
+  private lazy var scrollView = UIScrollView().then {
+    $0.isPagingEnabled = true
+    $0.showsHorizontalScrollIndicator = false
+    $0.bounces = false
+    $0.delegate = self
+  }
+  private lazy var pageControl = UIPageControl().then {
+    $0.numberOfPages = 3
+    $0.currentPage = 0
+    $0.pageIndicatorTintColor = .lightGray
+    $0.currentPageIndicatorTintColor = .gray
+    $0.transform = CGAffineTransform(scaleX: 0.6, y: 0.6)
+  }
+  private let nextButton = CompleteButton(isEnable: true)
+  
+  public init(reactor: R) {
+    self.onboardingReactor = reactor
+    super.init(nibName: nil, bundle: nil)
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  // MARK: - Lifecycle
+  
+  public override func viewDidLoad() {
+    super.viewDidLoad()
+    setAttribute()
+    setLayout()
+    self.reactor = onboardingReactor
+  }
+  
+  // MARK: - Layout
+  
+  public override func viewDidLayoutSubviews() {
+    super.viewDidLayoutSubviews()
+    rootContainer.pin.all(view.pin.safeArea)
+    rootContainer.flex.layout()
+    
+    scrollView.contentSize = CGSize(
+      width: scrollView.frame.width * 3,
+      height: scrollView.frame.height
+    )
+    scrollView.flex.layout(mode: .adjustHeight)
+  }
+  
+  public func setAttribute() {
+    view.backgroundColor = .white
+    view.addSubview(rootContainer)
+  }
+  
+  public func setLayout() {
+    rootContainer.flex
+      .define {
+        $0.addItem()
+          .justifyContent(.center)
+          .grow(1)
+          .define {
+            $0.addItem(scrollView)
+              .alignSelf(.center)
+            $0.addItem(pageControl)
+              .marginTop(57)
+              .height(5)
+          }
+        $0.addItem(nextButton)
+          .marginBottom(30)
+          .marginHorizontal(20)
+          .height(56)
+      }
+    
+    scrollView.flex
+      .direction(.row)
+      .define {
+        $0.addItem(descriptionView1)
+          .width(100%)
+          .height(80%)
+        $0.addItem(descriptionView2)
+          .width(100%)
+          .height(80%)
+        $0.addItem(descriptionView3)
+          .width(100%)
+          .height(80%)
+      }
+  }
+  
+  // MARK: - ScrollView Delegate
+  
+  public func scrollViewDidScroll(_ scrollView: UIScrollView) {
+    guard scrollView.frame.width > 0 else { return }
+    let pageIndex = round(scrollView.contentOffset.x / scrollView.frame.width)
+    pageControl.currentPage = Int(pageIndex)
+  }
+  
+}
+
+// MARK: - Binding
+
+extension OnboardingViewControllerImp: View {
+  public func bind(reactor: R) {
+    bindAction(reactor: reactor)
+    bindState(reactor: reactor)
+    bindEvent()
+  }
+  
+  public func bindAction(reactor: R) {
+    
+  }
+  
+  public func bindState(reactor: R) {
+    
+  }
+  
+  public func bindEvent() {
+    
+  }
+}
