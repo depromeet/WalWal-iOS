15a60fcec2ae451b727746670b9910982ca26b30
ir.__.si <cocoa9805@naver.com>
2024-08-01 18:40:15 +0900

    feat: #58 마이페이지 네비게이션 + 캘린더 연결

--- WalWal/Features/MyPage/MyPagePresenter/Implement/Views/MyPageViewImp.swift
+++ WalWal/Features/MyPage/MyPagePresenter/Implement/Views/MyPageViewImp.swift
@@ -9,6 +9,8 @@
 
 import UIKit
 import MyPagePresenter
+import DesignSystem
+import ResourceKit
 
 import Then
 import PinLayout
@@ -18,6 +20,20 @@ import RxSwift
 import RxCocoa
 
 public final class MyPageViewControllerImp<R: MyPageReactor>: UIViewController, MyPageViewController {
+  
+  // MARK: - UI
+  
+  private let containerView = UIView().then {
+    $0.backgroundColor = ResourceKitAsset.Colors.gray100.color
+  }
+  
+  private let navigationBar = WalWalNavigationBar(
+    leftItems: [],
+    title: "내 정보",
+    rightItems: [.setting]
+  ).then { $0.backgroundColor = ResourceKitAsset.Colors.white.color }
+  
+  private let calendar = WalWalCalendar(initialModels: [])
   
   public var disposeBag = DisposeBag()
   public var __reactor: R
@@ -41,14 +57,33 @@ public final class MyPageViewControllerImp<R: MyPageReactor>: UIViewController, 
     setLayout()
     self.reactor = __reactor
   }
-    
+  
+  public override func viewDidLayoutSubviews() {
+    super.viewDidLayoutSubviews()
+    containerView
+      .pin
+      .all(view.pin.safeArea)
+    containerView
+      .flex
+      .layout()
+  }
+   
+  // MARK: - Methods
+  
   
   public func setAttribute() {
     
   }
   
   public func setLayout() {
+    view.addSubview(containerView)
     
+    containerView.flex
+      .direction(.column)
+      .define { flex in
+        flex.addItem(navigationBar).height(60)
+        flex.addItem(calendar).grow(1)
+      }
   }
 }
 
