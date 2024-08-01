08eb7b37266cd30bdb4972db701c7736b0220f74
Zoe <mam07065@naver.com>
2024-08-02 04:06:55 +0900

    chore: #59 폰트 사용성을 높이기 위한 주석

--- WalWal/ResourceKit/Sources/Font.swift
+++ WalWal/ResourceKit/Sources/Font.swift
@@ -13,34 +13,49 @@ extension ResourceKitFontFamily {
   /// 사용 방법
   /// $0.font = ResourceKitFontFamily.KR.H1
   public struct KR {
+    /// 32px bold
     public static let H1 = UIFont.systemFont(ofSize: 32, weight: .bold)
+    /// 28px bold
     public static let H2 = UIFont.systemFont(ofSize: 28, weight: .bold)
+    /// 24px bold
     public static let H3 = UIFont.systemFont(ofSize: 24, weight: .bold)
+    /// 20px bold
     public static let H4 = UIFont.systemFont(ofSize: 20, weight: .bold)
+    /// 18px
     public struct H5 {
       public static let B = UIFont.systemFont(ofSize: 18, weight: .bold)
       public static let M = UIFont.systemFont(ofSize: 18, weight: .medium)
     }
+    /// 16px
     public struct H6 {
       public static let B = UIFont.systemFont(ofSize: 16, weight: .bold)
       public static let M = UIFont.systemFont(ofSize: 16, weight: .medium)
     }
+    /// 14px
     public struct H7 {
       public static let B = UIFont.systemFont(ofSize: 14, weight: .bold)
       public static let M = UIFont.systemFont(ofSize: 14, weight: .medium)
     }
+    /// 14px regular
     public static let B1 = UIFont.systemFont(ofSize: 14, weight: .regular)
+    /// 12px regular
     public static let B2 = UIFont.systemFont(ofSize: 12, weight: .regular)
+    /// 10px regular
     public static let Caption = UIFont.systemFont(ofSize: 10, weight: .regular)
   }
   
   /// 사용 방법
   /// $0.font = ResourceKitFontFamily.EN.H1
   public struct EN {
+    /// 16px bold
     public static let H1 = UIFont.systemFont(ofSize: 16, weight: .bold)
+    /// 14px semibold
     public static let H2 = UIFont.systemFont(ofSize: 14, weight: .semibold)
+    /// 14px medium
     public static let H3 = UIFont.systemFont(ofSize: 14, weight: .medium)
+    ///14px regular
     public static let B1 = UIFont.systemFont(ofSize: 14, weight: .regular)
+    /// 12px regular
     public static let Caption = UIFont.systemFont(ofSize: 12, weight: .regular)
   }
 }
