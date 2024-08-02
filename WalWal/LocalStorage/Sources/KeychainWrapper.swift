013fc6a7354df762ea0ccf15d549c5c499e9eb3e
ji-yeon224 <sjhymk@icloud.com>
2024-08-02 11:24:32 +0900

    키체인, UserDefaults 수정 (#66)
    * chore: LocalStorage 수정
    
    * chore: 불필요한 파일 제거

--- WalWal/LocalStorage/Sources/KeychainWrapper.swift
+++ WalWal/LocalStorage/Sources/KeychainWrapper.swift
@@ -11,14 +11,14 @@ import Foundation
 /// 키체인
 public final class KeychainWrapper {
   public static let shared = KeychainWrapper()
-
+  
   // MARK: - Key
-
+  
   public let userKey = "kr.co.walwal.user"
   public let UUIDKey = "kr.co.walwal.uuid"
-
+  
   // MARK: - deviceUUID
-
+  
   /// deviceUUID의 값을 접근합니다.
   /// KeychainWrapper.shared.deviceUUID
   public var deviceUUID: String {
@@ -30,22 +30,22 @@ public final class KeychainWrapper {
     ]
     var item: CFTypeRef?
     var status = SecItemCopyMatching(query as CFDictionary, &item)
-
+    
     if status == errSecSuccess,
        let existingItem = item as? Data,
        let uuid = String(data: existingItem, encoding: .utf8) {
       return uuid
     }
-
+    
     let deviceUUID = UUID().uuidString
     query = [
       kSecClass: kSecClassGenericPassword,
       kSecAttrService: UUIDKey,
       kSecValueData: deviceUUID.data(using: .utf8) as Any
     ]
-
+    
     status = SecItemAdd(query as CFDictionary, nil)
-
+    
     if status != errSecSuccess {
       /// UUID 저장에 실패한 경우, 기존 항목을 삭제하고 다시 시도
       if status == errSecDuplicateItem {
@@ -58,13 +58,13 @@ public final class KeychainWrapper {
     }
     return deviceUUID
   }
-
-  // MARK: - loginInfo
-
-  /// loingInfo의 authToken, userID에 접근이 가능합니다.
-  /// KeychainWrapper.shared.loginInfo?.authToken
-  /// KeychainWrapper.shared.loginInfo?.userId
-  public var loginInfo: LoginDTO? {
+  
+  // MARK: - AccessToken
+  
+  /// AccessToken에 접근이 가능합니다.
+  ///
+  /// `KeychainWrapper.shared.accessToken`
+  public var accessToken: String? {
     let query: [CFString: Any] = [
       kSecClass: kSecClassGenericPassword,
       kSecAttrService: userKey,
@@ -72,91 +72,86 @@ public final class KeychainWrapper {
       kSecReturnAttributes: true,
       kSecReturnData: true
     ]
-
+    
     var item: CFTypeRef?
     let status = SecItemCopyMatching(query as CFDictionary, &item)
-
+    
     if status == errSecSuccess,
        let existingItem = item as? [String: Any],
-       let authTokenData = existingItem[kSecValueData as String] as? Data,
-       let authToken = String(data: authTokenData, encoding: .utf8),
-       let userId = existingItem[kSecAttrAccount as String] as? String
+       let accessTokenData = existingItem[kSecValueData as String] as? Data,
+       let accessToken =  String(data: accessTokenData, encoding: .utf8)
     {
-      return LoginDTO(
-        userId: userId,
-        authToken: authToken
-      )
+      return accessToken
     } else {
-      print("키체인에서 LoginDTO 불러오기 실패: \(status)")
+      print("키체인에서 AccessToken 불러오기 실패: \(status)")
       return nil
     }
   }
-
+  
   /// loginInfo에 있는 프로퍼티의 값을 설정합니다.
-  /// KeychainWrapper.shared.setLoginInfo(.init(userId: "userID", authToken: "authToken"))
-  public func setLoginInfo(_ loginDTO: LoginDTO?) -> Bool {
-    if let loginDTO = loginDTO {
-      if loginInfo == nil {
-        return addLoginInfo(loginDTO)
+  ///
+  /// `KeychainWrapper.shared.setAccessToken("token")`
+  public func setAccessToken(_ accessToken: String?) -> Bool {
+    if let accessToken = accessToken {
+      if self.accessToken == nil {
+        return addAccessToken(accessToken)
       } else {
-        return updateLoginInfo(loginDTO)
+        return updateAccessToken(accessToken)
       }
     } else {
-      return deleteLoginInfo()
+      return deleteAccessToken()
     }
   }
-
-  /// 로그인 정보 키체인 저장 함수
-  private func addLoginInfo(_ loginDTO: LoginDTO) -> Bool {
+  
+  /// AccessToken 키체인 저장 함수
+  private func addAccessToken(_ accessToken: String) -> Bool {
     let query: [CFString: Any] = [
       kSecClass: kSecClassGenericPassword,
       kSecAttrService: userKey,
-      kSecAttrAccount: loginDTO.userId,
-      kSecValueData: loginDTO.authToken.data(using: .utf8) as Any,
-      kSecAttrLabel: "\(loginDTO.authToken)"
+      kSecAttrAccount: "AccessToken",
+      kSecValueData: (accessToken as AnyObject).data(using: String.Encoding.utf8.rawValue) as Any,
+      kSecAttrLabel: "AccessToken"
     ]
-
+    
     let status = SecItemAdd(query as CFDictionary, nil)
-
+    
     if status == errSecDuplicateItem {
-      return updateLoginInfo(loginDTO)
+      return updateAccessToken(accessToken)
     } else {
-      assert(status == errSecSuccess, "로그인 정보 키체인 저장 실패")
+      assert(status == errSecSuccess, "AccessToken 키체인 저장 실패")
       return status == errSecSuccess
     }
   }
-
-  /// 로그인 정보 키체인 업데이트 함수
-  private func updateLoginInfo(_ loginDTO: LoginDTO) -> Bool {
+  
+  /// AccessToken 키체인 업데이트 함수
+  private func updateAccessToken(_ accessToken: String) -> Bool {
     let prevQuery: [CFString: Any] = [
       kSecClass: kSecClassGenericPassword,
       kSecAttrService: userKey,
-      kSecAttrAccount: loginDTO.userId,
     ]
-
+    
     let updateQuery: [CFString: Any] = [
-      kSecAttrAccount: loginDTO.userId,
-      kSecValueData: loginDTO.authToken.data(using: .utf8) as Any,
-      kSecAttrLabel: "\(loginDTO.authToken)"
+      kSecValueData: (accessToken as AnyObject).data(using: String.Encoding.utf8.rawValue) as Any,
+      kSecAttrLabel: "AccessToken"
     ]
-
+    
     let status = SecItemUpdate(
       prevQuery as CFDictionary,
       updateQuery as CFDictionary
     )
-    assert(status == errSecSuccess, "로그인 정보 키체인 갱신 실패")
+    assert(status == errSecSuccess, "AccessToken 키체인 갱신 실패")
     return status == errSecSuccess
   }
-
-  /// 로그인 정보 키체인 삭제 함수
-  private func deleteLoginInfo() -> Bool {
+  
+  /// AccessToken 키체인 삭제 함수
+  private func deleteAccessToken() -> Bool {
     let query: [CFString: Any] = [
       kSecClass: kSecClassGenericPassword,
       kSecAttrService: userKey
     ]
-
+    
     let status = SecItemDelete(query as CFDictionary)
-    assert(status == errSecSuccess, "로그인 정보 키체인 삭제 실패")
+    assert(status == errSecSuccess, "AccessToken 키체인 삭제 실패")
     return status == errSecSuccess
   }
 }
