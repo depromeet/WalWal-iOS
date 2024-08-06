b708e8a5435749e213a1563da20e38a3e3922461
Zoe <mam07065@naver.com>
2024-08-06 15:24:19 +0900

    feat: #79 마이페이지 기록 화면전환

--- WalWal/Features/MyPage/MyPagePresenter/Interface/Reactors/MyPageReactor.swift
+++ WalWal/Features/MyPage/MyPagePresenter/Interface/Reactors/MyPageReactor.swift
@@ -8,22 +8,22 @@
 
 import MyPageDomain
 import MyPageCoordinator
+import DesignSystem
 
 import ReactorKit
 import RxSwift
 
 public enum MyPageReactorAction {
-  
+  case didSelectCalendarItem(WalWalCalendarModel)
 }
 
 public enum MyPageReactorMutation {
-  
+  case setSelectedCalendarItem(WalWalCalendarModel)
 }
 
 public struct MyPageReactorState {
-  public init() {
-  
-  }
+  public init() { }
+  public var selectedDate: WalWalCalendarModel = .init(imageId: "", date: "", imageData: .init())
 }
 
 public protocol MyPageReactor: Reactor where Action == MyPageReactorAction, Mutation == MyPageReactorMutation, State == MyPageReactorState {
