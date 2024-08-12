f33e9c342760e36c7112c5d7f10c8b0997160efa
Zoe <mam07065@naver.com>
2024-08-13 04:03:12 +0900

    feat: #103 피드뷰 구현

--- WalWal/Features/Feed/FeedPresenter/Implement/Views/FeedViewImp.swift
+++ WalWal/Features/Feed/FeedPresenter/Implement/Views/FeedViewImp.swift
@@ -9,6 +9,8 @@
 
 import UIKit
 import FeedPresenter
+import DesignSystem
+import ResourceKit
 
 import Then
 import PinLayout
@@ -19,13 +21,42 @@ import RxCocoa
 
 public final class FeedViewControllerImp<R: FeedReactor>: UIViewController, FeedViewController {
   
+  private typealias Images = ResourceKitAsset.Images
+  private typealias Colors = ResourceKitAsset.Colors
+  private typealias Fonts = ResourceKitFontFamily
+  
+  // MARK: - UI
+  
+  private let rootContainer = UIView()
+  
+  private let guideLabel = UILabel().then {
+    $0.font = Fonts.KR.H6.B
+    $0.textColor = .black
+    $0.text = "☀️ 다른 반려동물은 어떤 미션을 했을까요?"
+  }
+  
+  private let arrowIconImageView = UIImageView().then {
+    $0.image = Images.arrow.image
+  }
+  
+  private lazy var feed = WalWalFeed(feedData: dummyData, isFeed: true)
+  
+  // MARK: - Properties
+  
   public var disposeBag = DisposeBag()
-  public var __reactor: R
+  public var feedReactor: R
+  private let dummyData: [WalWalFeedModel] = [
+    .init(isFeedCell: true,
+          date: "", nickname: "멍", missionTitle: "산책미션을 완료했어요", profileImage: ResourceKitAsset.Sample.initialProfile.image,
+          missionImage: ResourceKitAsset.Sample.feedSample.image, boostCount: 123)
+  ]
+  
+  // MARK: - Initialize
   
   public init(
-      reactor: R
+    reactor: R
   ) {
-    self.__reactor = reactor
+    self.feedReactor = reactor
     super.init(nibName: nil, bundle: nil)
   }
   
@@ -37,18 +68,40 @@ public final class FeedViewControllerImp<R: FeedReactor>: UIViewController, Feed
   
   public override func viewDidLoad() {
     super.viewDidLoad()
-    setAttribute()
-    setLayout()
-    self.reactor = __reactor
-  }
-    
-  
-  public func setAttribute() {
-    
+    configureLayout()
+    configureAttribute()
+    self.reactor = feedReactor
   }
   
-  public func setLayout() {
-    
+  public override func viewDidLayoutSubviews() {
+    super.viewDidLayoutSubviews()
+    rootContainer.pin
+      .all(view.pin.safeArea)
+    rootContainer.flex
+      .layout()
+  }
+  
+  // MARK: - Layout
+  
+  public func configureAttribute() {
+    view.backgroundColor = Colors.gray150.color
+    view.addSubview(rootContainer)
+  }
+  
+  public func configureLayout() {
+    rootContainer.flex
+      .paddingTop(31)
+      .define {
+        $0.addItem(guideLabel)
+          .alignSelf(.center)
+        $0.addItem(arrowIconImageView)
+        
+          .alignSelf(.center)
+          .marginTop(14)
+          .size(24)
+        $0.addItem(feed)
+          .grow(1)
+      }
   }
 }
 
