b517927ceea81ddf1b1d70686dc1fe48f3dd910e
ji-yeon224 <69784492+ji-yeon224@users.noreply.github.com>
2024-08-17 01:13:16 +0900

    chore: 데모 뷰 빌드 오류 수정

--- WalWal/DesignSystem/DemoApp/Sources/WalWalFeed/WalWalFeedViewController.swift
+++ WalWal/DesignSystem/DemoApp/Sources/WalWalFeed/WalWalFeedViewController.swift
@@ -32,7 +32,6 @@ final class WalWalFeedViewController: UIViewController {
   
   private let dummyData: [WalWalFeedModel] = [
     .init(id: 1,
-          isFeedCell: false,
           date: "2024년 8월 10일",
           nickname: "찐찐도그",
           missionTitle: "산책 미션을 수행했어요!",
@@ -40,7 +39,6 @@ final class WalWalFeedViewController: UIViewController {
           missionImage: ResourceKitAsset.Sample.feedSample.image,
           boostCount: 324),
     .init(id: 2,
-          isFeedCell: true,
           date: "2024년 8월 10일",
           nickname: "찐찐도그",
           missionTitle: "산책 미션을 수행했어요!",
@@ -48,7 +46,6 @@ final class WalWalFeedViewController: UIViewController {
           missionImage: ResourceKitAsset.Sample.feedSample.image,
           boostCount: 324),
     .init(id: 3,
-          isFeedCell: false,
           date: "2024년 8월 10일",
           nickname: "찐찐도그",
           missionTitle: "산책 미션을 수행했어요!",
