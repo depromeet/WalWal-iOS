befca63775ca39801feac8d5eba72cb0d3da94d6
Zoe <mam07065@naver.com>
2024-08-19 06:24:54 +0900

    feat: #130 UseCase Reactor 연결

--- WalWal/Features/Feed/FeedPresenter/Implement/Reactors/FeedReactorImp.swift
+++ WalWal/Features/Feed/FeedPresenter/Implement/Reactors/FeedReactorImp.swift
@@ -6,6 +6,10 @@
 //  Created by 이지희
 //
 
+import UIKit
+import DesignSystem
+import GlobalState
+
 import FeedDomain
 import FeedPresenter
 import FeedCoordinator
@@ -14,25 +18,60 @@ import ReactorKit
 import RxSwift
 
 public final class FeedReactorImp: FeedReactor {
+
+  
   public typealias Action = FeedReactorAction
   public typealias Mutation = FeedReactorMutation
   public typealias State = FeedReactorState
   
   public let initialState: State
   public let coordinator: any FeedCoordinator
+  private let fetchFeedUseCase: FetchFeedUseCase
   
   public init(
-    coordinator: any FeedCoordinator
+    coordinator: any FeedCoordinator,
+    fetchFeedUseCase: FetchFeedUseCase
   ) {
     self.coordinator = coordinator
+    self.fetchFeedUseCase = fetchFeedUseCase
     self.initialState = State()
   }
   
   public func mutate(action: Action) -> Observable<Mutation> {
-    
+    switch action {
+    case .loadFeedData(let cursor, let limit):
+      return fetchFeedUseCase.excute(cursor: cursor, limit: limit)
+        .asObservable()
+        .map { feed -> [WalWalFeedModel] in
+          self.convertFeedModel(feedList: feed.list)
+        }
+        .map {
+          Mutation.setFeedData($0)
+        }
+    }
   }
   
   public func reduce(state: State, mutation: Mutation) -> State {
+    var newState = state
+    switch mutation {
+    case let .setFeedData(feed):
+      newState.feedData = feed
+    }
     
+    return newState
+  }
+  
+  private func convertFeedModel(feedList: [FeedListModel]) -> [WalWalFeedModel]{
+    return feedList.compactMap { feed in
+      let image = GlobalState.shared.imageStore[feed.missionRecordImageURL]
+      return WalWalFeedModel(
+        id: feed.missionID,
+        date: feed.createdDate,
+        nickname: "\(feed.authorID)",
+        missionTitle: "\(feed.missionID)",
+        profileImage: UIImage(),
+        missionImage: image ?? UIImage(),
+        boostCount: feed.totalBoostCount)
+    }
   }
 }
--- WalWal/Features/Feed/FeedPresenter/Interface/Reactors/FeedReactor.swift
+++ WalWal/Features/Feed/FeedPresenter/Interface/Reactors/FeedReactor.swift
@@ -8,29 +8,30 @@
 
 import FeedDomain
 import FeedCoordinator
+import DesignSystem
 
 import ReactorKit
 import RxSwift
 
 public enum FeedReactorAction {
-  
+  case loadFeedData(cursor: String, limits: Int)
 }
 
 public enum FeedReactorMutation {
-  
+ case setFeedData([WalWalFeedModel])
 }
 
 public struct FeedReactorState {
-  public init() {
-  
-  }
+  public init() {  }
+  public var feedData: [WalWalFeedModel] = []
 }
 
 public protocol FeedReactor: Reactor where Action == FeedReactorAction, Mutation == FeedReactorMutation, State == FeedReactorState {
-  
+
   var coordinator: any FeedCoordinator { get }
   
   init(
-    coordinator: any FeedCoordinator
+    coordinator: any FeedCoordinator,
+    fetchFeedUseCase: FetchFeedUseCase
   )
 }
