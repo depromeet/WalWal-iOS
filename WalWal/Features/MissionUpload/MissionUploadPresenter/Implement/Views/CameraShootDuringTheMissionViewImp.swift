df5be6b89341af5d9a97fe8ab5e4f5c466eb4255
ir.__.si <cocoa9805@naver.com>
2024-08-19 18:17:42 +0900

    feat: #131 사진촬영 뷰 UI 작업

--- WalWal/Features/MissionUpload/MissionUploadPresenter/Implement/Views/CameraShootDuringTheMissionViewImp.swift
+++ WalWal/Features/MissionUpload/MissionUploadPresenter/Implement/Views/CameraShootDuringTheMissionViewImp.swift
@@ -6,9 +6,11 @@
 //  Created by 조용인
 //
 
-
 import UIKit
 import MissionUploadPresenter
+import ResourceKit
+import DesignSystem
+import Utility
 
 import Then
 import PinLayout
@@ -19,13 +21,41 @@ import RxCocoa
 
 public final class MissionUploadViewControllerImp<R: CameraShootDuringTheMissionReactor>: UIViewController, CameraShootDuringTheMissionViewController {
   
+  private typealias Images = ResourceKitAsset.Images
+  private typealias Colors = ResourceKitAsset.Colors
+  private typealias Fonts = ResourceKitFontFamily
+  
   public var disposeBag = DisposeBag()
   public var cameraShootingDuringTheMissionReactor: R
+  
+  
+  private let navigationBar = WalWalNavigationBar(
+    leftItems: [.close],
+    title: nil,
+    rightItems: []
+  )
+  
+  private let cameraPreviewView = UIView()
+  
+  private let noticeLabelChip = WalWalChip(
+    text: "반려동물과 함께 산책한 사진을 찍어요!",
+    opacity: 0.9,
+    image: Images.flagS.image,
+    style: .semiFilled
+  )
+  
+  private var instructionLabel: UILabel!
+  private var captureButton: UIButton!
+  private var switchCameraButton: UIButton!
+  private var previewView: UIView!
+  
+  private let cameraManager: CameraManager
   
   public init(
       reactor: R
   ) {
     self.cameraShootingDuringTheMissionReactor = reactor
+    self.cameraManager = CameraManager()
     super.init(nibName: nil, bundle: nil)
   }
   
@@ -44,11 +74,12 @@ public final class MissionUploadViewControllerImp<R: CameraShootDuringTheMission
     
   
   public func setAttribute() {
-    
+   
   }
   
   public func setLayout() {
     
+    cameraManager.attachPreview(to: previewView)
   }
 }
 
@@ -63,14 +94,54 @@ extension MissionUploadViewControllerImp: View {
   }
   
   public func bindAction(reactor: R) {
+    // 사진 찍기 결과
+    cameraManager.photoCapturedSubject
+      .map{ Reactor.Action.photoCaptured($0)}
+      .bind(to: reactor.action)
+      .disposed(by: disposeBag)
     
+    // 카메라 전환 버튼 탭 처리
+    switchCameraButton.rx.tap
+      .subscribe(onNext: { [weak self] in
+        self?.cameraManager.switchCamera()
+      })
+      .disposed(by: disposeBag)
+    
+    // 닫기 버튼 탭 처리
+    closeButton.rx.tap
+      .subscribe(onNext: { [weak self] in
+        self?.dismiss(animated: true, completion: nil)
+      })
+      .disposed(by: disposeBag)
   }
   
   public func bindState(reactor: R) {
-    
+    reactor.state
+      .map { $0.capturedPhoto }
+      .distinctUntilChanged()
+      .compactMap { $0 }
+      .subscribe(onNext: { [weak self] image in
+        self?.handleCapturedPhoto(image: image)
+      })
+      .disposed(by: disposeBag)
   }
   
   public func bindEvent() {
+    // CameraManager의 사진 촬영 이벤트를 구독
+    captureButton.rx.tap
+      .subscribe(onNext: { [weak self] in
+        self?.cameraManager.capturePhoto()
+      })
+      .disposed(by: disposeBag)
     
+  }
+}
+
+extension MissionUploadViewControllerImp {
+  // 사진이 찍힌 후의 처리 로직 (예시로 이미지 저장 또는 표시)
+  private func handleCapturedPhoto(image: UIImage) {
+    let imageView = UIImageView(image: image)
+    imageView.frame = view.bounds
+    view.addSubview(imageView)
   }
 }
