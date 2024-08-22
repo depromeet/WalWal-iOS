562e281a87876fdd3ec58ac6c737bad87f5593e4
Zoe <mam07065@naver.com>
2024-08-23 08:36:52 +0900

    feat: #112 포맷팅

--- WalWal/Utility/Sources/String+.swift
+++ WalWal/Utility/Sources/String+.swift
@@ -14,4 +14,15 @@ public extension String {
     return NSPredicate(format: "SELF MATCHES %@", nicknameRegex)
           .evaluate(with: self)
   }
+  
+  /// 날짜 형식 변환 YYYY-MM-DD -> YYYY년 MM월 DD일
+  func toFormattedDate() -> String? {
+      let dateFormatter = DateFormatter()
+      dateFormatter.dateFormat = "yyyy-MM-dd"
+      guard let date = dateFormatter.date(from: self) else {
+          return nil
+      }
+      dateFormatter.dateFormat = "yyyy년 M월 d일"
+      return dateFormatter.string(from: date)
+  }
 }
