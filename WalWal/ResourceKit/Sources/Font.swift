0dfd6eb12672611ce5feb285462b5b3695f68510
ji-yeon224 <69784492+ji-yeon224@users.noreply.github.com>
2024-08-26 23:28:03 +0900

    chore: 줄간격 및 자간 적용

--- WalWal/ResourceKit/Sources/Font.swift
+++ WalWal/ResourceKit/Sources/Font.swift
@@ -84,15 +84,21 @@ extension ResourceKitFontFamily {
     else if font == Self.LotteriaChab.H1 { return 50 }
     else if font == Self.LotteriaChab.H2 { return 50 }
     else if font == Self.LotteriaChab.H3 { return 50 }
-    else if font == Self.KR.H1 { return 38 }
-    else if font == Self.KR.H2 { return 33 }
-    else if font == Self.KR.H3 { return 29 }
-    else if font == Self.KR.H4 { return 24 }
-    else if font == Self.KR.H5.B || font == Self.KR.H5.M { return 21 }
-    else if font == Self.KR.H6.B || font == Self.KR.H6.M || font == Self.EN.H1 { return 19 }
-    else if font == Self.KR.B2 || font == Self.EN.Caption{ return 14 }
-    else if font == Self.KR.Caption { return 12 }
-    else { return 17 }
+    else { return 0 }
+  }
+  
+  public static func lineHeightPercent(of font: ResourceKitFontConvertible.Font) -> CGFloat? {
+    if font == KR.H2 || font == KR.H6.B || font == KR.H6.M || font == KR.B2 {
+      return 130
+    } else {
+      return nil
+    }
+  }
+  
+  public static func spacingPercent(of font: ResourceKitFontConvertible.Font) -> CGFloat {
+    if font == Self.KR.B1 || font == Self.KR.B2 || font == Self.KR.Caption { return 0 }
+    else if font == Self.EN.B1 || font == Self.EN.B2 || font == Self.EN.Caption { return 0 }
+    else { return -2.5 }
   }
   
   public static func lineHeightPercent(of font: ResourceKitFontConvertible.Font) -> CGFloat? {
