ccd6c2b81fbc8dc4acf81a6495033d6d4b7a83bd
ji-yeon224 <69784492+ji-yeon224@users.noreply.github.com>
2024-10-02 15:34:22 +0900

    feat: #275 신고 타입 화면 UI 구성

--- WalWal/Features/Feed/FeedPresenter/Implement/Views/ReportView/ReportTypeViewImp.swift
+++ WalWal/Features/Feed/FeedPresenter/Implement/Views/ReportView/ReportTypeViewImp.swift
@@ -36,7 +36,38 @@ public final class ReportTypeViewControllerImp<R: ReportTypeReactor>:
     $0.isUserInteractionEnabled = true
   }
   
+  private let navTitle = CustomLabel(text: "신고", font: Fonts.KR.H5.B).then {
+    $0.textColor = Colors.black.color
+    $0.textAlignment = .center
+  }
   
+  
+  private let headerView = UIView()
+  
+  private let titleLabel = CustomLabel(
+    text: "이 게시물을 신고하려는 이유가 무엇인가요?",
+    font: Fonts.KR.H6.B
+  ).then {
+    $0.textColor = Colors.black.color
+    $0.textAlignment = .center
+  }
+  
+  private let descriptionLabel = CustomLabel(
+    text: "여러분의 신고 내용을 신중히 검토한 후,\n빠른 시일 내 적절한 조치를 취하겠습니다.",
+    font: Fonts.KR.B2
+  ).then {
+    $0.textColor = Colors.gray600.color
+    $0.numberOfLines = 2
+    $0.textAlignment = .center
+  }
+  
+  private let tableView = UITableView(frame: .zero, style: .plain).then {
+    $0.register(ReportTypeCell.self)
+    $0.rowHeight = 57.adjustedHeight
+    $0.separatorStyle = .none
+    $0.showsVerticalScrollIndicator = false
+    $0.isScrollEnabled = false
+  }
   
   // MARK: - Properties
   
@@ -45,7 +76,7 @@ public final class ReportTypeViewControllerImp<R: ReportTypeReactor>:
   private let sheetDownEvent = PublishRelay<Void>()
   private let endPanGesture = PublishRelay<CGPoint>()
   private let changePanGesture = PublishRelay<(CGPoint, CGPoint)>()
-  
+  private let reportItems = ReportType.allCases
   
   // MARK: - Initalize
   
@@ -54,6 +85,7 @@ public final class ReportTypeViewControllerImp<R: ReportTypeReactor>:
     super.init(nibName: nil, bundle: nil)
   }
   
+  @available(*, unavailable)
   required init?(coder: NSCoder) {
     fatalError("init(coder:) has not been implemented")
   }
@@ -62,11 +94,9 @@ public final class ReportTypeViewControllerImp<R: ReportTypeReactor>:
   
   public override func viewDidLoad() {
     super.viewDidLoad()
-    print(#function)
     self.reactor = reportTypeReactor
     configureAttribute()
     configureLayout()
-    
   }
   
   public override func viewDidAppear(_ animated: Bool) {
@@ -107,9 +137,25 @@ public final class ReportTypeViewControllerImp<R: ReportTypeReactor>:
       }
     
     rootContainer.flex
-      .height(300.adjusted)
-      .define { flex in
-
+      .paddingHorizontal(20.adjustedWidth)
+      .define {
+        $0.addItem(navTitle)
+          .marginTop(24.adjustedHeight)
+        $0.addItem(headerView)
+        $0.addItem(tableView)
+          .marginBottom(43.adjustedHeight)
+          .marginTop(11.adjustedHeight)
+      }
+    
+    headerView.flex
+      .paddingTop(30.adjustedHeight)
+      .paddingBottom(30.adjustedHeight)
+      .define {
+        $0.addItem(titleLabel)
+          .width(100%)
+        $0.addItem(descriptionLabel)
+          .width(100%)
+          .marginTop(8.adjusted)
       }
   }
   
@@ -213,6 +259,14 @@ extension ReportTypeViewControllerImp: View {
           owner.sheetDownEvent.accept(())
         }
       })
+      .disposed(by: disposeBag)
+    
+    Observable.just(reportItems)
+      .bind(to: tableView.rx.items(ReportTypeCell.self)) { index, data, cell in
+        cell.titleLabel.text = data.title
+        cell.isSeparatorHidden = index == self.reportItems.count-1
+        cell.selectionStyle = .none
+      }
       .disposed(by: disposeBag)
   }
 }
---
+++ WalWal/Features/Feed/FeedPresenter/Interface/Reactors/ReportTypeCellModel.swift
@@ -0,0 +1,14 @@
+//
+//  ReportTypeCellModel.swift
+//  FeedPresenter
+//
+//  Created by Jiyeon on 10/2/24.
+//  Copyright © 2024 olderStoneBed.io. All rights reserved.
+//
+
+import Foundation
+
+public struct ReportTypeCellModel: Hashable {
+  let type: ReportType
+  
+}
