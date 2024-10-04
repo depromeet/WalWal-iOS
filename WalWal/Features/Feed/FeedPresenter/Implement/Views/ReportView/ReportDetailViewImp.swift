91cb6f52c159d8b41adcfe7937f0bd5a8257c02b
ji-yeon224 <69784492+ji-yeon224@users.noreply.github.com>
2024-10-04 17:05:26 +0900

    feat: #275 바텀시트-전체화면 전환 구현

--- WalWal/Features/Feed/FeedPresenter/Implement/Views/ReportView/ReportDetailViewImp.swift
+++ WalWal/Features/Feed/FeedPresenter/Implement/Views/ReportView/ReportDetailViewImp.swift
@@ -37,15 +37,29 @@ public final class ReportDetailViewControllerImp<R: ReportDetailReactor>:
     $0.isUserInteractionEnabled = true
   }
   
-  private let navBar = UIView()
-  private let backButton = UIButton().then {
-    $0.setImage(Images.backS.image, for: .normal)
-  }
-  private let navTitle = CustomLabel(text: "신고", font: Fonts.KR.H5.B).then {
+  private let contentContainer = UIView()
+  
+  private let navigation = WalWalNavigationBar(leftItems: [.darkBack], leftItemSize: 32, title: "신고", rightItems: [])
+  
+  private let discriptionLabel = CustomLabel(
+    text: "구체적인 상황과 사유를 함께 적어주시면\n더 빠르고 정확하게 처리할 수 있어요! (선택)",
+    font: Fonts.KR.H6.B
+  ).then {
+    $0.numberOfLines = 2
     $0.textColor = Colors.black.color
     $0.textAlignment = .center
   }
-  private let navigation = WalWalNavigationBar(leftItems: [.darkBack], leftItemSize: 32, title: "신고", rightItems: [])
+  
+  private let textContainer = UIView()
+  
+  private let textView = UITextView().then {
+    $0.layer.cornerRadius = 10
+    $0.layer.borderWidth = 1
+    $0.layer.borderColor = Colors.gray300.color.cgColor
+    $0.textContainerInset = UIEdgeInsets(top: 16, left: 16, bottom: 34, right: 16)
+  }
+  
+  private let submitButton = WalWalButton(type: .dark, title: "왈왈팀에 전달하기")
   
   // MARK: - Properties
   
@@ -55,6 +69,7 @@ public final class ReportDetailViewControllerImp<R: ReportDetailReactor>:
   private let endPanGesture = PublishRelay<CGPoint>()
   private let changePanGesture = PublishRelay<(CGPoint, CGPoint)>()
   private let dismissView = PublishRelay<Void>()
+  private var isBottomSheet: Bool = true
   
   // MARK: - Initalize
   
@@ -82,16 +97,21 @@ public final class ReportDetailViewControllerImp<R: ReportDetailReactor>:
   
   public override func viewDidLayoutSubviews() {
     super.viewDidLayoutSubviews()
+    let _ = view.pin.keyboardArea.height
     dimView.pin
       .all()
     dimView.flex
       .layout()
+    contentContainer.flex
+      .marginBottom(self.view.pin.safeArea.bottom)
+      .layout()
     
     rootContainer.pin
-      .bottom(0)
+      .bottom()
       .horizontally(0)
       .height(563.adjustedHeight)
-    rootContainer.flex.layout(mode: .adjustHeight)
+    rootContainer.flex
+      .layout()
     
   }
   
@@ -102,6 +122,10 @@ public final class ReportDetailViewControllerImp<R: ReportDetailReactor>:
     rootContainer.layer.maskedCorners = CACornerMask(
       arrayLiteral: .layerMinXMinYCorner, .layerMaxXMinYCorner
     )
+    rootContainer.addSubview(contentContainer)
+    [navigation, discriptionLabel, textContainer, submitButton].forEach {
+      contentContainer.addSubview($0)
+    }
     
   }
   
@@ -116,16 +140,85 @@ public final class ReportDetailViewControllerImp<R: ReportDetailReactor>:
     
     rootContainer.flex
       .height(563.adjustedHeight)
+      .justifyContent(.start)
+    
+    contentContainer.flex
+      .grow(1)
+      .justifyContent(.spaceBetween)
+    
+    navigation.flex
+      .height(50.adjustedHeight)
+      .marginTop(24.adjustedHeight)
+      .width(100%)
+    
+    discriptionLabel.flex
+      .marginTop(15.adjustedHeight)
+      .marginBottom(30.adjustedHeight)
+      .marginHorizontal(20.adjustedWidth)
+    
+    textContainer.flex
+      .marginHorizontal(20.adjustedWidth)
+      .marginBottom(20.adjustedWidth)
+      .grow(1)
+    
+    submitButton.flex
+      .marginHorizontal(20.adjustedWidth)
+      .marginBottom(30.adjustedHeight)
+      .height(50.adjustedHeight)
+    
+    textContainer.flex
       .define {
-        $0.addItem(navigation)
-          .marginTop(24.adjustedHeight)
+        $0.addItem(textView)
+          .height(120.adjustedHeight)
           .width(100%)
       }
   }
   
+  private func changeFullPage() {
+    isBottomSheet = false
+    dimView.backgroundColor = Colors.white.color
+    
+    rootContainer.pin
+      .all()
+    contentContainer.flex
+      .markDirty()
+      .marginTop(view.pin.safeArea.top)
+      .bottom(0)
+      .layout()
+    submitButton.flex
+      .marginBottom(20.adjustedHeight)
+    rootContainer.flex
+      .layout()
+    view.layoutIfNeeded()
+  }
+  
+  private func changeBottomSheet() {
+    isBottomSheet = true
+    dimView.backgroundColor = Colors.black30.color
+    
+    contentContainer.flex
+      .markDirty()
+      .marginTop(0)
+      .marginBottom(self.view.pin.safeArea.bottom)
+      .layout()
+    
+    rootContainer.flex
+      .markDirty()
+    rootContainer.pin
+      .height(563.adjustedHeight)
+      .bottom()
+    submitButton.flex
+      .marginBottom(30.adjustedHeight)
+    rootContainer.flex
+      .layout()
+    view.layoutIfNeeded()
+  }
+  
+  
   // MARK: - Animations
   
   private func animateSheetUp() {
+    
     UIView.animate(withDuration: 0.3) {
       self.dimView.alpha = 1
       self.rootContainer.pin.bottom(0)
@@ -151,6 +244,8 @@ public final class ReportDetailViewControllerImp<R: ReportDetailReactor>:
   }
 }
 
+// MARK: - Binding
+
 extension ReportDetailViewControllerImp: View {
   public func bind(reactor: R) {
     bindAction(reactor: reactor)
@@ -158,7 +253,6 @@ extension ReportDetailViewControllerImp: View {
     bindEvent()
   }
   public func bindAction(reactor: R) {
-    
     changePanGesture
       .map {
         Reactor.Action.didPan(
@@ -202,6 +296,8 @@ extension ReportDetailViewControllerImp: View {
       .panGesture()
       .asObservable()
       .subscribe(with: self, onNext: { owner, gesture in
+        guard owner.isBottomSheet else { return }
+        
         let translation = gesture.translation(in: owner.rootContainer)
         let velocity = gesture.velocity(in: owner.rootContainer)
         
@@ -233,6 +329,23 @@ extension ReportDetailViewControllerImp: View {
     })
     .disposed(by: disposeBag)
     
+    rootContainer.rx.tapped
+      .bind(with: self) { owner, _ in
+        owner.textView.resignFirstResponder()
+      }
+      .disposed(by: disposeBag)
+    
+    NotificationCenter.default.rx.notification(UIResponder.keyboardWillShowNotification)
+      .bind(with: self) { owner, _ in
+        owner.changeFullPage()
+      }
+      .disposed(by: disposeBag)
+    
+    NotificationCenter.default.rx.notification(UIResponder.keyboardWillHideNotification)
+      .bind(with: self) { owner, _ in
+        owner.changeBottomSheet()
+      }
+      .disposed(by: disposeBag)
     
   }
 }
