0165d1d0bcd0053dfff9b299bdde2e289dab128d
ir.__.si <cocoa9805@naver.com>
2024-10-07 14:26:37 +0900

    feat: #281 Comment Domain Layer 구현 (대댓글 호출 API 연관)

---
+++ WalWal/Features/Comment/CommentDomain/Implement/PostCommentUsecaseImp.swift
@@ -0,0 +1,9 @@
+//
+//  PostCommentUsecaseImp.swift
+//  CommentDomain
+//
+//  Created by 조용인 on 10/7/24.
+//  Copyright © 2024 olderStoneBed.io. All rights reserved.
+//
+
+import Foundation
---
+++ WalWal/Features/Comment/CommentDomain/Interface/Model/GetCommentsModel.swift
@@ -0,0 +1,42 @@
+//
+//  GetCommentsModel.swift
+//  CommentDomain
+//
+//  Created by 조용인 on 10/7/24.
+//  Copyright © 2024 olderStoneBed.io. All rights reserved.
+//
+
+import Foundation
+import CommentData
+
+// MARK: - GetCommentsModel
+public struct GetCommentsModel {
+  public let comments: [CommentModel]
+  
+  public init(dto: GetCommentsDTO) {
+    self.comments = dto.comments.map { CommentModel(dto: $0) }
+  }
+}
+
+// MARK: - CommentModel
+public struct CommentModel {
+  public let parentID: Int?
+  public let commentID: Int
+  public let content: String
+  public let writerID: Int
+  public let writerNickname: String
+  public let writerProfileImageURL: String
+  public let createdAt: String
+  public let children: [CommentModel]
+  
+  public init(dto: Comment) {
+    self.parentID = dto.parentID
+    self.commentID = dto.commentID
+    self.content = dto.content
+    self.writerID = dto.writerID
+    self.writerNickname = dto.writerNickname
+    self.writerProfileImageURL = dto.writerProfileImageURL
+    self.createdAt = dto.createdAt
+    self.children = dto.children.map { CommentModel(dto: $0) }
+  }
+}
