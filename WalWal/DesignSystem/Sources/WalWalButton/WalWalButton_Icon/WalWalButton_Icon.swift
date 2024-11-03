eee97dae15069e886b3c3611b5d77fe3dcf95af8
Zoe <mam07065@naver.com>
2024-11-04 04:12:22 +0900

    feat: 디자인시스템 버튼 높이 조정

--- WalWal/DesignSystem/Sources/WalWalButton/WalWalButton_Icon/WalWalButton_Icon.swift
+++ WalWal/DesignSystem/Sources/WalWalButton/WalWalButton_Icon/WalWalButton_Icon.swift
@@ -29,6 +29,9 @@ public class WalWalButton_Icon: UIControl {
     var isEnabled: Bool
   }
   
+  private let buttonHeight = UIDevice.isSESizeDevice ? 40.adjustedSE : 50.adjusted
+  private let itemSize = UIDevice.isSESizeDevice ? 20.adjustedSE : 20.adjusted
+  
   // MARK: - UI
   
   private let rootView = UIView()
@@ -114,7 +117,7 @@ public class WalWalButton_Icon: UIControl {
     rootView.flex
       .justifyContent(.center)
       .alignItems(.center)
-      .height(50.adjusted)
+      .height(buttonHeight)
       .define { flex in
         flex.addItem(subContainer)
           .direction(.row)
@@ -122,7 +125,7 @@ public class WalWalButton_Icon: UIControl {
           .alignItems(.center)
           .define { flex in
             flex.addItem(iconImageView)
-              .size(20.adjusted)
+              .size(itemSize)
               .marginRight(2)
             flex.addItem(titleLabel)
               .height(20.adjusted)
